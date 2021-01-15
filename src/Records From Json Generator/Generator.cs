96b841ef402d6d94e1bd254548abad7829df351f
David Spörri <david.spoerri@coop.ch>
2021-01-15 17:23:51 +0100

    first version of project. generator is not working.

---
+++ src/Records From Json Generator/Generator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace Records_From_Json_Generator
+{
+    [Generator]
+    public class Generator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AddSource("first test.cs", "using System;\nnamespace Demo {\n\tpublic class Test\n\t{\n\t\tpublic static string Hi => \"Hello, World!!\";\n\t}\n}");
+        }
+    }
+}
